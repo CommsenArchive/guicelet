47145fa5f3f731f4b8022c31da52a6fec92d17c6
Milen Dyankov <milendyankov@gmail.com>
2011-11-19 21:03:37 +0100

    abit more advanced test case

--- src/test/java/com/commsen/guicelet/test/DispatcherTest.java
+++ src/test/java/com/commsen/guicelet/test/DispatcherTest.java
@@ -12,11 +12,15 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.client.methods.HttpOptions;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.methods.HttpTrace;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.eclipse.jetty.server.Server;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -27,14 +31,13 @@ import com.commsen.guicelet.test.util.TestContext;
 
 public class DispatcherTest {
 
-	private static final String ECHO = "ECHO\n";
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {		
+	public static void setUpBeforeClass() throws Exception {
 		TestContext.addRoot("/g/");
 		TestContext.addGuicelet(SimpleGuicelet.class);
 		JettyServer.start();
-		
+
 	}
 
 	@AfterClass
@@ -42,43 +45,62 @@ public class DispatcherTest {
 		JettyServer.stop();
 	}
 
-	
 	@Test
 	public void test() throws Exception {
 
-		
 		HttpClient httpclient = new DefaultHttpClient();
 
+		// test all methods
 		String url = JettyServer.getHost() + "/g/echo";
-//		testRequest(httpclient, new HttpDelete(url));
-		testRequest(httpclient, new HttpGet(url));
-//		testRequest(httpclient, new HttpHead(url));
-//		testRequest(httpclient, new HttpOptions(url));
-		testRequest(httpclient, new HttpPost(url));
-//		testRequest(httpclient, new HttpPut(url));
-//		testRequest(httpclient, new HttpTrace(url));
+		testRequest(httpclient, new HttpDelete(url), HttpServletResponse.SC_OK);
+		testRequest(httpclient, new HttpGet(url), HttpServletResponse.SC_OK, SimpleGuicelet.ECHO);
+		testRequest(httpclient, new HttpHead(url), HttpServletResponse.SC_OK);
+		testRequest(httpclient, new HttpOptions(url), HttpServletResponse.SC_OK);
+		testRequest(httpclient, new HttpPost(url), HttpServletResponse.SC_OK, SimpleGuicelet.ECHO);
+		testRequest(httpclient, new HttpPut(url), HttpServletResponse.SC_OK);
+		testRequest(httpclient, new HttpTrace(url), HttpServletResponse.SC_OK);
+
+		// test trailing slash
+		testRequest(httpclient, new HttpGet("/g/test"), HttpServletResponse.SC_OK, SimpleGuicelet.TEST);
+		testRequest(httpclient, new HttpGet("/g/test/"), HttpServletResponse.SC_OK, SimpleGuicelet.TEST);
+
+		// test second level mapping
+		testRequest(httpclient, new HttpGet("/g/test/test2"), HttpServletResponse.SC_OK, SimpleGuicelet.TEST2);
 		
+		// test not found 
+		testRequest(httpclient, new HttpDelete("/g/test"), HttpServletResponse.SC_NOT_FOUND);
+		testRequest(httpclient, new HttpHead("/g/test"), HttpServletResponse.SC_NOT_FOUND);
+		testRequest(httpclient, new HttpOptions("/g/test"), HttpServletResponse.SC_NOT_FOUND);
+		testRequest(httpclient, new HttpPost("/g/test"), HttpServletResponse.SC_NOT_FOUND);
+		testRequest(httpclient, new HttpPut("/g/test"), HttpServletResponse.SC_NOT_FOUND);
+		testRequest(httpclient, new HttpTrace("/g/test"), HttpServletResponse.SC_NOT_FOUND);
 	}
 
+	private void testRequest(HttpClient httpclient, HttpRequestBase httpRequest, int expectedCode) throws ClientProtocolException, IOException {
+		testRequest(httpclient, httpRequest, expectedCode, null);
+	}
 
-	private void testRequest(HttpClient httpclient, HttpRequestBase httpRequest)
-			throws IOException, ClientProtocolException {
+	private void testRequest(HttpClient httpclient, HttpRequestBase httpRequest, int expectedCode, String expectedResponse) throws IOException,
+			ClientProtocolException {
 		HttpResponse response = httpclient.execute(httpRequest);
-		Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatusLine().getStatusCode());
-		Assert.assertNotNull(response.getEntity());
-		Assert.assertEquals(ECHO, getResponse(response.getEntity()));
+		Assert.assertEquals(expectedCode, response.getStatusLine().getStatusCode());
+		String method = httpRequest.getMethod();
+		if (expectedResponse != null && ("GET".equals(method) || "POST".equals(method))) {
+			Assert.assertNotNull(response.getEntity());
+			Assert.assertEquals(expectedResponse, getResponse(response.getEntity()));
+		}
 	}
-
 
 	private String getResponse(HttpEntity entity) throws IOException {
 		StringBuilder sb = new StringBuilder();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
 		String l;
+		int i = 0;
 		while ((l = reader.readLine()) != null) {
-			sb.append(l).append("\n");
+			if (i++ > 0) sb.append("\n");
+			sb.append(l);
 		}
 		return sb.toString();
 	}
-
 
 }
--- src/test/java/com/commsen/guicelet/test/guicelets/SimpleGuicelet.java
+++ src/test/java/com/commsen/guicelet/test/guicelets/SimpleGuicelet.java
@@ -7,32 +7,28 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.commsen.guicelet.Guicelet;
 import com.commsen.guicelet.GuiceletRequest;
-import com.commsen.guicelet.HttpMethod;
-import com.google.inject.Inject;
-import com.google.inject.name.Named;
+import static com.commsen.guicelet.HttpMethod.*;
 
 @Guicelet
 public class SimpleGuicelet {
 	
+	public static final String ECHO = "ECHO";
+	public static final String TEST = "TEST";
+	public static final String TEST2 = "TEST2";
 
-	@GuiceletRequest(path="/echo", methods={HttpMethod.GET, HttpMethod.POST, HttpMethod.HEAD})
+	@GuiceletRequest(path="/echo", methods={DELETE, GET, HEAD, OPTIONS, POST, PUT, TRACE})
 	public void echo(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.getWriter().print("ECHO");
+		response.getWriter().print(ECHO);
 	}
 
-	@GuiceletRequest(path="/test", methods={HttpMethod.GET})
+	@GuiceletRequest(path="/test", methods={GET})
 	public void sayHelloGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.getWriter().print("GET");
+		response.getWriter().print(TEST);
 	}
 
-	@GuiceletRequest(path="/test", methods={HttpMethod.POST})
+	@GuiceletRequest(path="/test/test2", methods={POST})
 	public void sayHelloPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.getWriter().print("POST");
+		response.getWriter().print(TEST2);
 	}
 
-	@GuiceletRequest(path="/test", methods={HttpMethod.POST})
-	public void a
-	(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.getWriter().print("POST");
-	}
 }
